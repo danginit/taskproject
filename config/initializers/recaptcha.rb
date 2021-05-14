0c565408d9a6762b4ff5f2e71dd7bf272fb47b10
Nitesh Dangi <niteshdangi80@gmail.com>
2021-05-14 17:35:01 +0530

    First commit

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,16 @@
+Recaptcha.configure do |config|
+    # config.site_key  = '6Ld05M8aAAAAALYOzXNCnXDO3yUsfBIsb7HZLMgu'
+    # config.secret_key = '6Ld05M8aAAAAAHxRsR5Sp4zhAJOYAkBGZoq-d5c-'
+
+
+    config.site_key  = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
+    config.secret_key = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
+  
+    # Uncomment the following line if you are using a proxy server:
+    # config.proxy = 'http://myproxy.com.au:8080'
+  
+    # Uncomment the following lines if you are using the Enterprise API:
+    # config.enterprise = true
+    # config.enterprise_api_key = 'AIzvFyE3TU-g4K_Kozr9F1smEzZSGBVOfLKyupA'
+    # config.enterprise_project_id = 'my-project'
+end
---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  root to: 'home#index'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
+  resources :users do
+    member do
+      get :confirm_email
+    end
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
